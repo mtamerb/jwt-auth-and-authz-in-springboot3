8361443ade848cd2fef635fe5d4695e3ccda19f6
tamer bilici <tamerbilici00@gmail.com>
2023-10-26 19:22:14 +0300

    Initial commit

---
+++ src/main/java/com/example/jwtauthandauthzinspringboot3/JwtAuthAndAuthzInSpringboot3Application.java
@@ -0,0 +1,13 @@
+package com.example.jwtauthandauthzinspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthAndAuthzInSpringboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthAndAuthzInSpringboot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jwtauthandauthzinspringboot3/JwtAuthAndAuthzInSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtauthandauthzinspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthAndAuthzInSpringboot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
