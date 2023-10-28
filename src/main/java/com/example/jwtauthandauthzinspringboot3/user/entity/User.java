5fb6d0130c1805c6132ea8d5a71fcdd128530aef
tamer bilici <tamerbilici00@gmail.com>
2023-10-28 21:56:49 +0300

    entity

---
+++ src/main/java/com/example/jwtauthandauthzinspringboot3/user/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.jwtauthandauthzinspringboot3.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String roles;
+}
