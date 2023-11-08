4d7b88194c8d1e418c962837b12d1f8c1cf8f076
tamer bilici <tamerbilici00@gmail.com>
2023-11-08 22:46:24 +0300

    req

---
+++ src/main/java/com/example/jwtauthandauthzinspringboot3/auth/request/RegisterRequest.java
@@ -0,0 +1,21 @@
+package com.example.jwtauthandauthzinspringboot3.auth.request;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
